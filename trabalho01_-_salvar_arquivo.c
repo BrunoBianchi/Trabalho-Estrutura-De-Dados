16d863c1d46d6097796e419c9fc328074917edb9
Bruno Bianchi <bruno.raiadobian@gmail.com>
2023-10-08 17:16:03 -0300

    Remvover

--- trabalho01_-_salvar_arquivo.c
+++ trabalho01_-_salvar_arquivo.c
@@ -1,57 +1,64 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct Aluno{
-	char nome[30];
-	int matricula;
-	int idade; 
-	int nroDisciplinas;
-	struct Aluno *prox;
-}Aluno;
+typedef struct Aluno
+{
+    char nome[30];
+    int matricula;
+    int idade;
+    int nroDisciplinas;
+    struct Aluno *prox;
+} Aluno;
 
-typedef struct Universidade{
-	char nome[30];
-	int qtdAlunos;
-	struct Universidade *prox;
-	struct Aluno *inicioAluno;
-}Universidade;
+typedef struct Universidade
+{
+    char nome[30];
+    int qtdAlunos;
+    struct Universidade *prox;
+    struct Aluno *inicioAluno;
+} Universidade;
 
-
-void insere(Universidade** inicio, char nome[], int qtdAlunos) {
-    Universidade* nova_uni = malloc(sizeof(Universidade));
+void insere(Universidade **inicio, char nome[], int qtdAlunos)
+{
+    Universidade *nova_uni = malloc(sizeof(Universidade));
     strcpy(nova_uni->nome, nome);
     nova_uni->qtdAlunos = qtdAlunos;
     nova_uni->prox = NULL;
     nova_uni->inicioAluno = NULL;
 
-
-    if (*inicio == NULL) {
+    if (*inicio == NULL)
+    {
         *inicio = nova_uni;
         return;
     }
 
-    Universidade* aux = *inicio;
-    while (aux->prox != NULL) {
+    Universidade *aux = *inicio;
+    while (aux->prox != NULL)
+    {
         aux = aux->prox;
     }
     aux->prox = nova_uni;
 }
 
-void salvaDados(Universidade* inicio) {
-    FILE* file = fopen("lista_uni.txt", "w");
-    if (file == NULL) {
+void salvaDados(Universidade *inicio)
+{
+    FILE *file = fopen("lista_uni.txt", "w");
+    if (file == NULL)
+    {
         printf("Criando arquivo.\n");
         return;
     }
 
-    Universidade* auxUniv = inicio;
-    while (auxUniv != NULL) {
+    Universidade *auxUniv = inicio;
+    while (auxUniv != NULL)
+    {
         fprintf(file, "%s\n", auxUniv->nome);
         fprintf(file, "%d\n", auxUniv->qtdAlunos);
 
-        Aluno* auxAluno = auxUniv->inicioAluno;
-        while (auxAluno != NULL) {
+        Aluno *auxAluno = auxUniv->inicioAluno;
+        while (auxAluno != NULL)
+        {
             fprintf(file, "%s\n", auxAluno->nome);
             fprintf(file, "%d\n", auxAluno->matricula);
             fprintf(file, "%d\n", auxAluno->idade);
@@ -66,27 +73,74 @@ void salvaDados(Universidade* inicio) {
     fclose(file);
 }
 
-Universidade* recuperaDados(Universidade **inicio) {
-    FILE* file = fopen("lista_uni.txt", "r");
-    if (file == NULL) {
+void removerUniversidade(Universidade **inicio)
+{
+    printf("Digite o nome da universidade:\n");
+    char universidadeName[30];
+    scanf("%s", &universidadeName);
+    universidadeName[strlen(universidadeName) + 1] = '\0';
+    Universidade *aux = *inicio;
+
+    // Percorrer as universidades ate encontrar um valor de aux que tenha o nome da universidade
+    while (aux->prox != NULL)
+    {
+
+        if (strcmp(aux->nome, universidadeName) == 0)
+        {
+            break;
+        }
+        aux = aux->prox;
+    }
+    aux->nome[strlen(aux->nome) + 1] = '\0';
+    if (aux->prox == NULL && strcmp(aux->nome, universidadeName) != 0 || strcmp(aux->nome, universidadeName) != 0)
+    {
+        printf("Universidade nao encontrada!\n");
+        return;
+    }
+    // Percorrer todos os alunos e ir dando free neles!
+    Aluno *aux_Aluno = aux->inicioAluno;
+    Aluno *aux_Aluno_anterior = aux->inicioAluno;
+    if (aux_Aluno != NULL)
+    {
+        while (aux_Aluno->prox != NULL)
+        {
+            aux_Aluno_anterior = aux_Aluno;
+            aux_Aluno = aux_Aluno->prox;
+            free(aux_Aluno_anterior);
+        }
+    }
+
+    free(aux);
+    printf("Universidade Deletada!");
+}
+
+Universidade *recuperaDados(Universidade **inicio)
+{
+    FILE *file = fopen("lista_uni.txt", "r");
+    if (file == NULL)
+    {
         printf("Arquivo inexistente, sera criado.\n");
         return NULL;
     }
 
-    Universidade* auxUniv = NULL;
-    Aluno* auxAluno = NULL;
+    Universidade *auxUniv = NULL;
+    Aluno *auxAluno = NULL;
     char line[30];
 
-    while (fgets(line, sizeof(line), file) != NULL) {
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
         line[strcspn(line, "\n")] = '\0';
 
-        if (strlen(line) == 0) {
+        if (strlen(line) == 0)
+        {
             auxUniv->prox = NULL;
             auxUniv = NULL;
             auxAluno = NULL;
-        } else if (auxUniv == NULL) {
+        }
+        else if (auxUniv == NULL)
+        {
             // Create a new university entry
-            Universidade* nova_uni = malloc(sizeof(Universidade));
+            Universidade *nova_uni = malloc(sizeof(Universidade));
             strcpy(nova_uni->nome, line);
 
             fgets(line, sizeof(line), file);
@@ -95,19 +149,25 @@ Universidade* recuperaDados(Universidade **inicio) {
             nova_uni->prox = NULL;
             nova_uni->inicioAluno = NULL;
 
-            if (*inicio == NULL) {
+            if (*inicio == NULL)
+            {
                 *inicio = nova_uni;
-            } else {
-            	auxUniv = *inicio;
-            	while(auxUniv->prox != NULL){
-            		auxUniv = auxUniv->prox;
-				}
+            }
+            else
+            {
+                auxUniv = *inicio;
+                while (auxUniv->prox != NULL)
+                {
+                    auxUniv = auxUniv->prox;
+                }
                 auxUniv->prox = nova_uni;
             }
             auxUniv = nova_uni;
-        } else {
+        }
+        else
+        {
             // Create a new student entry
-            Aluno* novo_aluno = malloc(sizeof(Aluno));
+            Aluno *novo_aluno = malloc(sizeof(Aluno));
             strcpy(novo_aluno->nome, line);
 
             fgets(line, sizeof(line), file);
@@ -121,9 +181,12 @@ Universidade* recuperaDados(Universidade **inicio) {
 
             novo_aluno->prox = NULL;
 
-            if (auxAluno == NULL) {
+            if (auxAluno == NULL)
+            {
                 auxUniv->inicioAluno = novo_aluno;
-            } else {
+            }
+            else
+            {
                 auxAluno->prox = novo_aluno;
             }
             auxAluno = novo_aluno;
@@ -134,47 +197,55 @@ Universidade* recuperaDados(Universidade **inicio) {
     return *inicio;
 }
 
-void imprime(Universidade* head){
-	if(head == NULL){
-		printf("Lista vazia");
-		return;
-	}
-	
-	Universidade *aux = head;
-	while(aux != NULL){
-		printf("%s ", aux->nome);
-		aux = aux->prox;
-	}
+void imprime(Universidade *head)
+{
+    if (head == NULL)
+    {
+        printf("Lista vazia");
+        return;
+    }
+
+    Universidade *aux = head;
+    while (aux != NULL)
+    {
+        printf("%s ", aux->nome);
+        aux = aux->prox;
+    }
 }
 
-int main() {
+int main()
+{
     Universidade *inicio = NULL;
-	int op;
-	recuperaDados(&inicio);
-	char nome[30];
-	
-	//imprime(inicio);
+    int op;
+    recuperaDados(&inicio);
+    char nome[30];
 
-	printf("\n\ndeseja inserir mais?");
-	scanf("%d", &op);
+    // imprime(inicio);
 
-	while(op == 1){
-		fflush(stdin);
-    	printf("insira uma universidade:");
-    	gets(nome);
-    	
-    	fflush(stdin);
-    	
-    	insere(&inicio, nome, 0);
-    	
-    	printf("deseja inserir mais?");
-    	scanf("%d", &op);
-    	
-	}
-	
-	imprime(inicio);
-	
-    salvaDados(inicio);
+    while (op != 0)
+    {
+        printf("\n\nMenu:\n1.Inserir Uma Nova Unverdidade\n2.Inserir Aluno\n3.Busca Universidade\n4.Busca Aluno\n5.Remove Universidade\n6.Remover Aluno\n0.Fechar\n");
+        scanf("%d", &op);
+
+        switch (op)
+        {
+        case 1:
+            fflush(stdin);
+            printf("insira uma universidade:");
+            gets(nome);
+            fflush(stdin);
+            insere(&inicio, nome, 0);
+            printf("deseja inserir mais?");
+            scanf("%d", &op);
+            salvaDados(inicio);
+            break;
+        case 2:
+            removerUniversidade(&inicio);
+            break;
+        default:
+            printf("Menu:\n1.Inserir Uma Nova Unverdidade\n2.Inserir Aluno\n3.Busca Universidade\n4.Busca Aluno\n5.Remove Universidade\n6.Remover Aluno\n0.Fechar\n");
+        }
+    }
 
     return 0;
 }
